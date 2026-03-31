ebefaa7be3e4ec80c111296799047dc279bb82b6
Lautaro Trueba <lautarotr2002@gmail.com>
2026-03-31 11:43:20 -0300

    Mejoro ejercicio2

--- Practica 1/ejercicio2.py
+++ Practica 1/ejercicio2.py
@@ -9,4 +9,23 @@ rest = seconds % 3600
 minutes = rest // 60
 secs = rest % 60 
 
-print(f'{seconds} segundos equivale a {hours} horas, {minutes} minutos y {secs} segundos')
+if (seconds == 1):
+    word_seconds = "segundo"
+else:
+    word_seconds = "segundos"
+
+if (hours == 1):
+    word_h = "hora"
+else:
+    word_h = "horas"
+
+if (minutes == 1):
+    word_m = "minuto"
+else:
+    word_m = "minutos"
+
+if (secs == 1):
+    word_s = "segundo"
+else:
+    word_s = "segundos"
+print(f'{seconds} {word_seconds} equivale a {hours} {word_h}, {minutes} {word_m} y {secs} {word_s}.')
